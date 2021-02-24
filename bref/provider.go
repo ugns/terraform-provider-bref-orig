b24b9ea1eab4fb7cc29a86980c6aedb012eb8a43
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2021-02-24 00:00:54 -0500

    Initial commit

---
+++ bref/provider.go
@@ -0,0 +1,13 @@
+package bref
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Provider -
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap:   map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{},
+	}
+}
